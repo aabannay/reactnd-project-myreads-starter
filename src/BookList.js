4bf0f4cc87166ba681b084f7ae7afc6d53692d5a
Abdullah Albannay <aabannay@gmail.com>
2018-05-19 16:36:35 +0300

    Show books in shelf using components

--- src/BookList.js
+++ src/BookList.js
@@ -4,16 +4,21 @@ import BookShelf from './BookShelf'
 class BookList extends Component {
 
   render() {
+    const books = this.props.books;
+    const currentlyReadingBooks = books.filter((book) => book.shelf === 'currentlyReading');
+    const wantToReadBooks = books.filter((book) => book.shelf === 'wantToRead');
+    const readBooks = books.filter((book) => book.shelf === 'read');
+
     return (
       <div className="list-books">
         <div className="list-books-title">
-          <h1>MyReads</h1>
+          <h1>{this.props.title}</h1>
         </div>
         <div className="list-books-content">
           <div>
-            <BookShelf/>
-            <BookShelf/>
-            <BookShelf/>
+            <BookShelf title='Currently Reading' books={currentlyReadingBooks}/>
+            <BookShelf title='Want to Read' books={wantToReadBooks}/>
+            <BookShelf title='Read' books={readBooks}/>
           </div>
         </div>
       </div>
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import Book from './Book'
+
 class BookShelf extends Component {
   render () {
     return (
       <div>
         <div className="bookshelf">
-          <h2 className="bookshelf-title">Currently Reading</h2>
+          <h2 className="bookshelf-title">{this.props.title}</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              <li>
-                <Book/>
-              </li>
-              <li>
-                <Book/>
-              </li>
+              {this.props.books.map((book) => (
+                <li key={book.id}>
+                  <Book book={book}/>
+                </li>
+              ))}
             </ol>
           </div>
         </div>
